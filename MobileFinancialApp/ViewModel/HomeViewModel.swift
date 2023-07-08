abca7ba28a9e19bc397db18f9d6dca9a1ead8ebc
Ahsen Bahtışen <65489789+ahsen-bahtisen@users.noreply.github.com>
2023-07-08 10:45:07 +0300

    Total calculation

--- MobileFinancialApp/ViewModel/HomeViewModel.swift
+++ MobileFinancialApp/ViewModel/HomeViewModel.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
-import FirebaseFirestore
-
+import Firebase
 
 class HomeViewModel {
     
-   
+    func fetchTotalBudget(completion: @escaping (Double) -> Void) {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            completion(0)
+            return
+        }
+        
+        let ref = Database.database().reference().child("users").child(userId)
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            if let userData = snapshot.value as? [String: Any],
+               let totalBudget = userData["totalBudget"] as? Double {
+                completion(totalBudget)
+            } else {
+                completion(0)
+            }
+        }
+    }
 }
-
 
 
